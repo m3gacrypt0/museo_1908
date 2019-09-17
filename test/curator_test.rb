2c194ef7b7c8f161545462849369d96185aaba25
Allison E. Moore <m3gacrypt0@gmail.com>
2019-09-17 09:29:09 -0600

    Update curator_test.rb with tests for add_photographs and add_artists

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -1,11 +1,39 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/curator'
+require './lib/photograph'
+require './lib/artist'
 
 class CuratorTest < Minitest::Test
 
   def setup
     @curator = Curator.new
+    @photo_1 = Photograph.new({
+         id: "1",
+         name: "Rue Mouffetard, Paris (Boy with Bottles)",
+         artist_id: "1",
+         year: "1954"
+    })
+    @photo_2 = Photograph.new({
+         id: "2",
+         name: "Moonrise, Hernandez",
+         artist_id: "2",
+         year: "1941"
+    })
+    @artist_1 = Artist.new({
+        id: "1",
+        name: "Henri Cartier-Bresson",
+        born: "1908",
+        died: "2004",
+        country: "France"
+    })
+    @artist_2 = Artist.new({
+        id: "2",
+        name: "Ansel Adams",
+        born: "1902",
+        died: "1984",
+        country: "United States"
+    })
   end
 
   def test_it_exists
@@ -15,6 +43,20 @@ class CuratorTest < Minitest::Test
   def test_it_has_intitial_attributes
     assert_equal [], @curator.photographs
     assert_equal [], @curator.artists
+  end
+
+  def test_method_add_photograph
+    @curator.add_photograph(@photo_1)
+    assert_equal [@photo_1], @curator.photographs
+    @curator.add_photograph(@photo_2)
+    assert_equal [@photo_1, @photo_2], @curator.photographs
+  end
+
+  def test_method_add_artist
+    @curator.add_artist(@artist_1)
+    assert_equal [@artist_1], @curator.artists
+    @curator.add_artist(@artist_2)
+    assert_equal [@artist_1, @artist_2], @curator.artists
   end
 
 end
